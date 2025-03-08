848ed9680f1133016dd0ac1bcf13280164db4843
Matthew Antone <matt.antone@gmail.com>
2025-03-08 10:43:45 -0800

    removed unused types

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,18 +1,12 @@
 import { home } from './types/home-type';
 import { page } from './types/page-type';
-import { model } from './types/models-type';
-import { unit } from './types/units';
-import { finish } from './types/finishes-type';
 import { blockContent } from './types/blockContent';
 import { settings } from './types/settings-type';
 import * as blocks from './blocks'
-console.log(blocks)
+
 export const schemaTypes = [
   home,
   page,
-  model,
-  unit,
-  finish,
   blockContent,
   settings,
   ...Object.values(blocks)
